3dfe74d3b4da7831333229f69837b36258728bb9
apisak.k <>
2020-06-29 16:56:01 +0700

    cpc_result bug

--- login/module/report_admin/ajax-query-update_score3.php
+++ login/module/report_admin/ajax-query-update_score3.php
@@ -5,10 +5,12 @@ include_once "../../includes/dbconn.php";
 include_once "../report/class-report.php";
 include_once "../myClass.php";
 
-$updateResult = array("success" => NULL,
-                        "result" => NULL,
-                        "msg" => NULL,
-                        "log" => NULL );
+$updateResult = array(
+    "success" => NULL,
+    "result" => NULL,
+    "msg" => NULL,
+    "log" => NULL
+);
 
 $report = new report;
 $myClass = new myClass;
@@ -21,58 +23,61 @@ $personalTable = $currentYear['data']['per_personal'];
 $kpiScoreResultTable = $currentYear['data']['kpi_score_result'];
 $cpcScoreResultTable = $currentYear['data']['cpc_score_result'];
 
-$cpcTypeKey = array(1,2,3,4,5,6);
-$msg_result = $_POST['per_cardno']."";
+$cpcTypeKey = array(1, 2, 3);
+$cpcTypeKey2 = array(1, 2, 3, 4, 5, 6);
+$msg_result = $_POST['per_cardno'] . "";
 $per_cardno = $_POST['per_cardno'];
 // $per_cardno = '1729900270641';
 $msg_error = "";
 $log = array();
 $log_ = array();
-$doneScore = $report->CPCscoreDone($per_cardno,$cpcScoreTable,$year);
+$doneScore = $report->CPCscoreDone($per_cardno, $cpcScoreTable, $year);
 if ($doneScore['result'] === 0) {
 
-    (!empty($per_cardno)? $cpcResult =  $report->tableCPC($per_cardno,$year,$cpcTypeKey,$personalTable,$cpcScoreTable) : $cpcResult);
+    (!empty($per_cardno) ? $cpcResult =  $report->tableCPC($per_cardno, $year, $cpcTypeKey, $personalTable, $cpcScoreTable) : $cpcResult);
     $cpc = $report->reportCPC1($cpcResult);
-    $updateResutl = $report->cpcResultUpdate($cpc,$currentYear['data']);
+    $updateResutl = $report->cpcResultUpdate($cpc, $currentYear['data']);
 
-    $r = $report->cal_gap_chart($cpcResult);
+    (!empty($per_cardno) ? $cpcResult2 =  $report->tableCPC($per_cardno, $year, $cpcTypeKey2, $personalTable, $cpcScoreTable) : $cpcResult);
+    $r = $report->cal_gap_chart($cpcResult2);
 
     $gapUpdate = array();
 
     foreach ($r as $keyGapUpdate => $valueGapUpdate) {
 
-        $gapUpdate[] = $report->gapUpdateByid($valueGapUpdate['pointEqual_OverGap'],$valueGapUpdate['gap_status'],$valueGapUpdate['cpc_score_id'],$cpcScoreTable);
-
+        $gapUpdate[] = $report->gapUpdateByid($valueGapUpdate['pointEqual_OverGap'], $valueGapUpdate['gap_status'], $valueGapUpdate['cpc_score_id'], $cpcScoreTable);
     }
     $log_['updateResutl'] =  $updateResutl;
     $log_['gapUpdate'] = $gapUpdate;
     $msg_result .= " <span class='text text-success'> สมรรถนะ ประเมินเสร็จแล้ว </span>";
-}else {
-    $report->cpcUpdateScoreResult($cpcScoreResultTable, $per_cardno, NULL, NULL,  NULL ,  $year ,  NULL);
+} else {
+    $report->cpcUpdateScoreResult($cpcScoreResultTable, $per_cardno, NULL, NULL,  NULL,  $year,  NULL);
 
-    $msg_result .= " <span class='text text-warning'> สมรรถนะ ยังประเมินไม่เสร็จ</span>" ;
+    $msg_result .= " <span class='text text-warning'> สมรรถนะ ยังประเมินไม่เสร็จ</span>";
 }
 
 
-$kpiDone = $report->KPIscoreDone($per_cardno,$kpiScoreTable,$year);
-if ($kpiDone['success'] == true && $kpiDone['result'] === 0 && $kpiDone['checkaccept'] === 0 ) {
+$kpiDone = $report->KPIscoreDone($per_cardno, $kpiScoreTable, $year);
+if ($kpiDone['success'] == true && $kpiDone['result'] === 0 && $kpiDone['checkaccept'] === 0) {
 
-    (!empty($per_cardno)? $kpiResult = $report->tableKPI($per_cardno,$year,$personalTable,$kpiScoreTable) : $kpiResult);
+    (!empty($per_cardno) ? $kpiResult = $report->tableKPI($per_cardno, $year, $personalTable, $kpiScoreTable) : $kpiResult);
     $kpi = $report->reportKPI1($kpiResult);
-    $kpiResultUpdate =  $report->kpiResultUpdate($kpi,$currentYear['data']);
+    $kpiResultUpdate =  $report->kpiResultUpdate($kpi, $currentYear['data']);
 
     $msg_result .= "<span class='text text-info'> ตัวชี้วัด ยังประเมินเสร็จแล้ว</span>";
     $log_['kpiResultUpdate'] = $kpiResultUpdate;
-}else {
-    $report->KPIupdateScoreResult( $kpiScoreResultTable,  $per_cardno,  NULL,  NULL,  $year,  NULL);
-    $msg_result .= " <span class='text text-warning'> ตัวชี้วัด ยังประเมินไม่เสร็จ</span>" ;
+} else {
+    $report->KPIupdateScoreResult($kpiScoreResultTable,  $per_cardno,  NULL,  NULL,  $year,  NULL);
+    $msg_result .= " <span class='text text-warning'> ตัวชี้วัด ยังประเมินไม่เสร็จ</span>";
 }
 
 $log[] = $log_;
 
-$updateResult = array("success" => true,
-                        "result" => $msg_result,
-                        "msg" => null,
-                        "log" => $log);
+$updateResult = array(
+    "success" => true,
+    "result" => $msg_result,
+    "msg" => null,
+    "log" => $log
+);
 
 echo json_encode($updateResult);
--- login/process_score2.php
+++ login/process_score2.php
@@ -17,68 +17,66 @@ $personalTable = $currentYear['data']['per_personal'];
 $kpiScoreResultTable = $currentYear['data']['kpi_score_result'];
 $cpcScoreResultTable = $currentYear['data']['cpc_score_result'];
 
-$cpcTypeKey = array(1,2,3,4,5,6);
+$cpcTypeKey = array(1, 2, 3);
+$cpcTypeKey2 = array(1, 2, 3, 4, 5, 6);
+
 
 $msg_error = "";
 $log = array();
 $log_ = array();
 
-   $sqlPersonal = "select per_cardno from $personalTable ";
-   $stm = $db->conn->prepare($sqlPersonal);
-   $stm->execute();
-   $personalResult = $stm->fetchAll(PDO::FETCH_ASSOC);
-    
- 
+$sqlPersonal = "select per_cardno from $personalTable ";
+$stm = $db->conn->prepare($sqlPersonal);
+$stm->execute();
+$personalResult = $stm->fetchAll(PDO::FETCH_ASSOC);
+
+
 foreach ($personalResult as $key => $value) {
 
     $per_cardno = $value['per_cardno'];
     $msg_result = array();
     $msg_result_ = $value['per_cardno'];
 
-    $doneScore = $report->CPCscoreDone($per_cardno,$cpcScoreTable,$year);
+    $doneScore = $report->CPCscoreDone($per_cardno, $cpcScoreTable, $year);
     if ($doneScore['result'] === 0) {
 
-        (!empty($per_cardno)? $cpcResult =  $report->tableCPC($per_cardno,$year,$cpcTypeKey,$personalTable,$cpcScoreTable) : $cpcResult);
+        (!empty($per_cardno) ? $cpcResult =  $report->tableCPC($per_cardno, $year, $cpcTypeKey, $personalTable, $cpcScoreTable) : $cpcResult);
         $cpc = $report->reportCPC1($cpcResult);
-        $updateResutl = $report->cpcResultUpdate($cpc,$currentYear['data']);
+        $updateResutl = $report->cpcResultUpdate($cpc, $currentYear['data']);
 
-        $r = $report->cal_gap_chart($cpcResult);
-
+        (!empty($per_cardno) ? $cpcResult2 =  $report->tableCPC($per_cardno, $year, $cpcTypeKey2, $personalTable, $cpcScoreTable) : $cpcResult);
+        $r = $report->cal_gap_chart($cpcResult2);
         $gapUpdate = array();
-
         foreach ($r as $keyGapUpdate => $valueGapUpdate) {
 
-            $gapUpdate[] = $report->gapUpdateByid($valueGapUpdate['pointEqual_OverGap'],$valueGapUpdate['gap_status'],$valueGapUpdate['cpc_score_id'],$cpcScoreTable);
-
+            $gapUpdate[] = $report->gapUpdateByid($valueGapUpdate['pointEqual_OverGap'], $valueGapUpdate['gap_status'], $valueGapUpdate['cpc_score_id'], $cpcScoreTable);
         }
         $log_['updateResutl'] =  $updateResutl;
         $log_['gapUpdate'] = $gapUpdate;
         $msg_result_ .= " สมรรถนะ ประเมินเสร็จแล้ว ";
-    }else {
-        $report->cpcUpdateScoreResult($cpcScoreResultTable, $per_cardno, NULL, NULL,  NULL ,  $year ,  NULL);
+    } else {
+        $report->cpcUpdateScoreResult($cpcScoreResultTable, $per_cardno, NULL, NULL,  NULL,  $year,  NULL);
 
-        $msg_result_ .= "  สมรรถนะ ยังประเมินไม่เสร็จ" ;
+        $msg_result_ .= "  สมรรถนะ ยังประเมินไม่เสร็จ";
     }
 
 
-    $kpiDone = $report->KPIscoreDone($per_cardno,$kpiScoreTable,$year);
-    if ($kpiDone['success'] == true && $kpiDone['result'] === 0 && $kpiDone['checkaccept'] === 0 ) {
+    $kpiDone = $report->KPIscoreDone($per_cardno, $kpiScoreTable, $year);
+    if ($kpiDone['success'] == true && $kpiDone['result'] === 0 && $kpiDone['checkaccept'] === 0) {
 
-        (!empty($per_cardno)? $kpiResult = $report->tableKPI($per_cardno,$year,$personalTable,$kpiScoreTable) : $kpiResult);
+        (!empty($per_cardno) ? $kpiResult = $report->tableKPI($per_cardno, $year, $personalTable, $kpiScoreTable) : $kpiResult);
         $kpi = $report->reportKPI1($kpiResult);
-        $kpiResultUpdate =  $report->kpiResultUpdate($kpi,$currentYear['data']);
+        $kpiResultUpdate =  $report->kpiResultUpdate($kpi, $currentYear['data']);
 
         $msg_result_ .= " ตัวชี้วัด ยังประเมินเสร็จแล้ว";
         $log_['kpiResultUpdate'] = $kpiResultUpdate;
-    }else {
-        $report->KPIupdateScoreResult( $kpiScoreResultTable,  $per_cardno,  NULL,  NULL,  $year,  NULL);
-        $msg_result_ .= "  ตัวชี้วัด ยังประเมินไม่เสร็จ" ;
+    } else {
+        $report->KPIupdateScoreResult($kpiScoreResultTable,  $per_cardno,  NULL,  NULL,  $year,  NULL);
+        $msg_result_ .= "  ตัวชี้วัด ยังประเมินไม่เสร็จ";
     }
 
     $log[] = $log_;
     $msg_result[] = $msg_result_;
 
-    printf($key." ".$msg_result_."\n");
-
+    printf($key . " " . $msg_result_ . "\n");
 }
-
